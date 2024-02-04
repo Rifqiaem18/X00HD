927e93144960f14afb4b716959a425035701ec19
Rifqi <64751889+Rifqiaem18@users.noreply.github.com>
2024-02-05 01:56:38 +0700

    Update omni_X00HD.mk

--- omni_X00HD.mk
+++ omni_X00HD.mk
@@ -1,7 +1,7 @@
 # Specify phone tech before including full_phone
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-$(call inherit-product, device/xiaomi/rolex/full_X00HD.mk)
+$(call inherit-product, device/asus/X00HD/full_X00HD.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
